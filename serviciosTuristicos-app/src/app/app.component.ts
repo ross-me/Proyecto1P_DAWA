1ae8393622038654860c83f794a97a83b764d04a
ross-me <adelaocampo516@gmail.com>
2024-11-24 14:32:10 -0500

    correccion

--- serviciosTuristicos-app/src/app/app.component.ts
+++ serviciosTuristicos-app/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { ListaPeliculasComponent } from './components/lista-peliculas/lista-peliculas.component';
-import { Pelicula } from '../models/Pelicula';
-import { PeliculasjsonService } from './services/peliculasjson.service';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListaPeliculasComponent, RouterLink,
+  imports: [RouterOutlet, RouterLink,
     RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
