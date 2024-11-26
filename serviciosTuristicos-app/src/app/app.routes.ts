135feabb1738121ec3637f7a0fa70de7f58f02e2
ross-me <adelaocampo516@gmail.com>
2024-11-25 20:04:42 -0500

    index

--- serviciosTuristicos-app/src/app/app.routes.ts
+++ serviciosTuristicos-app/src/app/app.routes.ts
@@ -10,6 +10,7 @@ import { ListaRestaurantesComponent } from './components/lista-restaurantes/list
 import { ListaExcursionesComponent } from './components/lista-excursiones/lista-excursiones.component';
 import { CrudExcursionesComponent } from './components/crud-excursiones/crud-excursiones.component';
 import { CrudAlojamientosComponent } from './components/crud-alojamientos/crud-alojamientos.component';
+import { IndexComponent } from './components/index/index.component';
 
 
 export const routes: Routes = [
@@ -23,12 +24,13 @@ export const routes: Routes = [
     {path:"crud-guias-turisticos", component: CrudGuiasTuristicosComponent},
     {path:"crud-restaurantes", component: CrudRestaurantesComponent},
     {path:"crud-excursiones", component: CrudExcursionesComponent},
-    {path:"crud-alojamientos", component: CrudAlojamientosComponent}
+    {path:"crud-alojamientos", component: CrudAlojamientosComponent},
+    {path:"index", component: IndexComponent},
     
     //rutas por defecto
     
-    //{path:"", redirectTo: "index-servicios", pathMatch:'full'}, //no existe
-    //{path:"**", redirectTo: "index-servicios"} //equivocada el "**" representa cualquier otra ruta q 
+    {path:"", redirectTo: "index", pathMatch:'full'}, //no existe
+    {path:"**", redirectTo: "index"} //equivocada el "**" representa cualquier otra ruta q 
     //esté dentro de las colocadas arriba
 
 ];
--- serviciosTuristicos-app/src/app/components/index/index.component.ts
+++ serviciosTuristicos-app/src/app/components/index/index.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule, RouterLink ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
